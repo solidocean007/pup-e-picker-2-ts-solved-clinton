bcec21e1f2ef8e2b7de96a98cb8cc9e93ef28146
solidocean007 <clintonwilliams007@gmail.com>
2023-08-30 21:14:58 -0400

    setup apli calls

--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,35 @@
+export const baseUrl = "http://localhost:3000";
+// import { TNewDog } from "./Functional/FunctionalCreateDogForm";
+import { Dog } from "./types";
+
 const getAllDogs = () => {
-  // fill out method
+  return fetch(`${baseUrl}/dogs`).then((response) => response.json());
 };
 
-const postDog = () => {
-  // fill out method
-};
-const deleteDogRequest = () => {
-  // fill out method
-};
+const postDog = (dog: Omit<Dog, 'id'>) => {
+  return fetch(`${baseUrl}/dogs`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dog)
+  }).then(response => response.json())
+}
 
-const patchFavoriteForDog = () => {
-  // fill out method
+const deleteDogRequest = (id: number) => {
+  return fetch(`${baseUrl}/dogs/${id}`, {
+    method: "DELETE",
+  });
+}
+
+const patchFavoriteForDog = (id: number, updatedDog: Dog) => {
+  return fetch(`${baseUrl}/dogs/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedDog),
+  }).then((response) => response.json());
 };
 
 export const Requests = {
